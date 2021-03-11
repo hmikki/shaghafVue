6fe8ce8bfa00729eac850be7bf43ee6b65327124
hmikki <30619974+hmikki@users.noreply.github.com>
2021-03-11 16:01:42 +0200

    updated

--- src/components/sections/payment_form.vue
+++ src/components/sections/payment_form.vue
@@ -1,10 +1,20 @@
 <template>
   <form action="{shopperResultUrl}" class="paymentWidgets" data-brands="VISA MASTER AMEX"></form>
 </template>
-<script src="https://test.oppwa.com/v1/paymentWidgets.js?checkoutId=${checkoutId}"></script>
 
 <script>
 let checkoutId = sessionStorage.getItem('payment_token');
 import 'https://test.oppwa.com/v1/paymentWidgets.js?checkoutId=';
+export default {
+  data(){
+    return{
 
+    }
+  },
+  created() {
+  },
+  methods:{
+
+  }
+}
 </script>
